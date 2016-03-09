e5c9d97118710db946074677f9d33d5b0884eca3
quadstar <icberg27@gmail.com>
2016-03-09 17:20:30 -0600

    bundling javascript on server

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express    = require('express');
 var app        = express();
 var port       = process.env.PORT || 8080;
+var browserify = require('browserify-middleware');
+var Path       = require('path');
 
-app.use(express.static(__dirname));
+browserify.settings({ transform: ['reactify']});
+
+app.get('/bundle.js', browserify('./client/main.js'));
+
+var assetFolder = Path.resolve(__dirname, './client');
+
+app.use(express.static(assetFolder));
+
+app.get('/*', function(req, res){
+  res.sendFile(assetFolder + '/index.html');
+});
 
 app.listen(port);
 console.log('Server Running on ' + port);
