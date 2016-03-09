90070f72339a52443fe1d981ea9cdd6c7b83fa3b
quadstar <icberg27@gmail.com>
2016-03-09 17:26:01 -0600

    cleaned up server file

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ var port       = process.env.PORT || 8080;
 var browserify = require('browserify-middleware');
 var Path       = require('path');
 
-browserify.settings({ transform: ['reactify']});
+// route to check board validation. 
+app.post('./')
 
+// convert jsx to js and bundle files
+browserify.settings({ transform: ['reactify']});
 app.get('/bundle.js', browserify('./client/main.js'));
 
+// Host static files and handle wildcard routes.
 var assetFolder = Path.resolve(__dirname, './client');
-
 app.use(express.static(assetFolder));
-
 app.get('/*', function(req, res){
   res.sendFile(assetFolder + '/index.html');
 });
 
+// start server
 app.listen(port);
 console.log('Server Running on ' + port);
