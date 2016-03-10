0ea91e4dfacdf8c39faf1f918887aeab5aa84865
quadstar <icberg27@gmail.com>
2016-03-10 09:27:58 -0600

    added basic click functionality.

--- client/main.js
+++ client/main.js
@@ -21,11 +21,19 @@ var Main = React.createClass({
   getInitialState: function(){
     return {
       turn: 'X',
-      rows: [[0,0,0],[0,0,0],[0,0,0]]
+      rows: [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
     };
   },
   clickSquare: function(event){
-    console.log('Clicked square #', event.target.id);
+    console.log('Clicked square #', event.target.id); 
+    var row = parseInt(event.target.id[0]);
+    var col = parseInt(event.target.id[1]);
+    var newRows = this.state.rows.slice();
+    newRows[row][col] = this.state.turn;
+    this.setState({
+      turn: this.state.turn === 'X' ? 'O' : 'X',
+      rows: newRows
+    });
   },
   render: function(){
     var clickfn = this.clickSquare;
@@ -33,9 +41,9 @@ var Main = React.createClass({
       <div>
         {this.state.rows.map(function(row,rowi, c){
           return (
-            <div key={rowi} id='row'>
+            <div className='row' key={rowi} id='row'>
             {row.map(function(e,i){
-              return (<Square clickSquare={clickfn} key={rowi*c.length+i} id={rowi*c.length+i} />);
+              return (<Square marker={e} clickSquare={clickfn} key={rowi*c.length+i} id={''+rowi+i} />);
             })}
             </div> 
           );
--- index.js
+++ index.js
@@ -4,16 +4,21 @@ var port       = process.env.PORT || 8080;
 var browserify = require('browserify-middleware');
 var Path       = require('path');
 
-// route to check board validation. 
-app.post('./')
+// route to check board for win condition. 
+app.post('./api/board-check', function(req, res){
+
+});
 
 // convert jsx to js and bundle files
 browserify.settings({ transform: ['reactify']});
+
 app.get('/bundle.js', browserify('./client/main.js'));
 
 // Host static files and handle wildcard routes.
 var assetFolder = Path.resolve(__dirname, './client');
+
 app.use(express.static(assetFolder));
+
 app.get('/*', function(req, res){
   res.sendFile(assetFolder + '/index.html');
 });
